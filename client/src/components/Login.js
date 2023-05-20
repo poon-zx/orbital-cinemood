25962b5618e5e4bc022e51569cc3e18a58d221f5
poon-zx <poonzhexuan@gmail.com>
2023-05-20 16:40:25 +0800

    edit Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,14 @@ export const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZWNhZmZveG54bmFod2ZjcGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1NjY1MTAsImV4cCI6MjAwMDE0MjUxMH0.-W7IPp668Pp4uT5ZwzAawRU7fJYj20_6MXGOm06VDgA'
 )
 const Login = () => (
-  <Container maxWidth="xs" sx={{ height: "100vh", justifyContent: "center" }}>
+  <Container
+  style={{ background:'#ffffff'}}
+  maxWidth="xs"
+  display="flex"
+  justifyContent="sssscenter"
+  alignItems="center"
+  minHeight="100vh"
+>
     <img src={logo} alt="Logo" height="80"/>
     <Auth
       supabaseClient={supabase}
