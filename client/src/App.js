eac647916a2c83b0731b73d71b4500436c558d33
poon-zx <poonzhexuan@gmail.com>
2023-05-20 15:36:59 +0800

    create connection to Supabase

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,23 @@ import React, {useState} from 'react';
 import Login from './components/Login';
 import Register from './components/Register';
 import './App.css';
+import { Auth } from '@supabase/auth-ui-react'
+import { createClient } from '@supabase/supabase-js'
 
+const supabase = createClient(
+  'https://ccecaffoxnxnahwfcpcy.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZWNhZmZveG54bmFod2ZjcGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1NjY1MTAsImV4cCI6MjAwMDE0MjUxMH0.-W7IPp668Pp4uT5ZwzAawRU7fJYj20_6MXGOm06VDgA'
+)
+const App = () => (
+  <Auth
+    supabaseClient={supabase}
+    className='App'
+  />
+)
+
+export default App;
+
+/*
 function App() {
     const [currentForm, setCurrentForm] = useState('login');
     const toggleForm = (formName) => {
@@ -17,5 +33,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAtOJOsuZD9GYImdN-WN8t1AW1YphdW1WQ",
-  authDomain: "cinemood-ddcb5.firebaseapp.com",
-  projectId: "cinemood-ddcb5",
-  storageBucket: "cinemood-ddcb5.appspot.com",
-  messagingSenderId: "832612297119",
-  appId: "1:832612297119:web:194c9eb9b56892414e35aa",
-  measurementId: "G-DPJ8GEEYNS"
-};
-
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
