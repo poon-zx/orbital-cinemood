51e32c64d3b51206b10c93bc5de6cd052b3b9aa3
poon-zx <poonzhexuan@gmail.com>
2023-06-09 21:49:13 +0800

    edit

--- src/Model/model.py
+++ src/Model/model.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer, util
 import torch
-import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
