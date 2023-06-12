6fd9a10edfd1bf2d4ef5df674f81f2ea36a2867f
poon-zx <poonzhexuan@gmail.com>
2023-06-12 21:18:36 +0800

    reduce columns

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ import requests
 url_csv = "https://storage.googleapis.com/cinemood/Overall%20Movie.csv"
 response_csv = requests.get(url_csv)
 data = StringIO(response_csv.text)
-dataset = pd.read_csv(data)
+dataset = pd.read_csv(data, usecols = ['Movie Name, Year of Release, Movie Rating'])
 
 tensors = torch.load('Overall Movies.pt')
 
