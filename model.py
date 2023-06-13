5cb1b934dc940ad5d681d3c3b4bafeba58e7d923
poon-zx <poonzhexuan@gmail.com>
2023-06-14 00:08:51 +0800

    edit

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from transformers import AutoModel
+from sentence_transformers import SentenceTransformer, util
 import torch
 import pandas as pd
 from io import StringIO
@@ -14,7 +14,7 @@ CORS(app)
 def serve():
         return app.send_static_file('index.html')
 
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+model = SentenceTransformer('all-MiniLM-L6-v2')
 model.max_seq_length = 384
 
 import requests
