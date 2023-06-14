6228420bea904b8d1e802d83c9eff8894b2b09a5
poon-zx <poonzhexuan@gmail.com>
2023-06-14 16:31:40 +0800

    change pt to google cloud

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin
 from sentence_transformers import SentenceTransformer, util
 import torch
 import pandas as pd
-from io import StringIO
+from io import StringIO, BytesIO
 import os
 import requests
 
@@ -14,7 +14,7 @@ CORS(app)
 def serve():
         return app.send_static_file('index.html')
 
-model = SentenceTransformer('paraphrase-MiniLM-L3-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
 
 import requests
 
@@ -23,7 +23,9 @@ response_csv = requests.get(url_csv)
 data = StringIO(response_csv.text)
 dataset = pd.read_csv(data)
 
-tensors = torch.load('Overall Movie.pt')
+url_tensors = "https://storage.googleapis.com/cinemood/Overall%20Movies.pt"
+response_tensors = requests.get(url_tensors)
+tensors = torch.load(BytesIO(response_tensors.content))
 
 @app.route("/find_similarity/", methods=['POST', 'OPTIONS'])
 @cross_origin(options=None)
@@ -34,12 +36,12 @@ def find_similarity():
     input = input.replace("[^a-zA-Z#]", " ")
     embeddings1 = model.encode(input, convert_to_tensor=True)
     cosine_scores = util.pytorch_cos_sim(embeddings1, tensors)
-    top_results = torch.topk(cosine_scores, k=20)
+    top_results = torch.topk(cosine_scores, k=30)
     top_indices = top_results[1][0]
     top_scores = top_results[0][0]
 
     results = []
-    for i in range(20):
+    for i in range(30):
         results.append({
             'movie': dataset['Movie Name'][top_indices[i].item()],
             'score': float(top_scores[i].item()),
