0cb69490f56efc0d0d5b281922d5e84af76e39bb
poon-zx <poonzhexuan@gmail.com>
2023-06-16 14:10:12 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from io import StringIO, BytesIO
 import os
 import requests
 
-app = Flask(__name__, static_folder='build', static_url_path='/')
+app = Flask(__name__, static_folder='build/static')
 CORS(app)
 
 @app.route('/', defaults={'path': ''})
