9b3c892390fe0988eb399ad33992e097e36bea16
poon-zx <poonzhexuan@gmail.com>
2023-06-21 17:53:26 +0800

    Update header.test.js

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter, useLocation, Router } from "react-router-dom";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import {
+  MemoryRouter,
+  useLocation,
+  Router,
+  BrowserRouter,
+} from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import Header from "../components/Header/header.js";
 import { SearchContext } from "../context/SearchContext.js"; // import your SearchContext
 import { act } from "react-dom/test-utils";
+import { createMemoryHistory } from "history";
 
 // mock setSearchText function
 const mockSetSearchText = jest.fn();
@@ -31,36 +37,100 @@ describe("Header", () => {
     );
 
     act(() => {
-    expect(screen.getByAltText("Home")).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: "Popular" })).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: "Top Rated" })).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: "Upcoming" })).toBeInTheDocument();
-    expect(
-      screen.getByPlaceholderText("search movie name...")
-    ).toBeInTheDocument();
+      expect(screen.getByAltText("Home")).toBeInTheDocument();
+      expect(screen.getByRole("link", { name: "Popular" })).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: "Top Rated" })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: "Upcoming" })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByPlaceholderText("search movie name...")
+      ).toBeInTheDocument();
     });
   });
 
-  it("navigates to the home page when Home link is clicked", () => {
+  test("clicking home logo redirects to home page", () => {
+    const history = createMemoryHistory();
+
+    global.window = { location: { pathname: null } };
+
     render(
-      <MemoryRouter initialEntries={["/search"]}>
+      <BrowserRouter history={history}>
+        <SearchContext.Provider value={{ mockSetSearchText }}>
+          <Header />
+        </SearchContext.Provider>
+      </BrowserRouter>
+    );
+
+    const logo = screen.getByAltText("Home");
+    fireEvent.click(logo);
+
+    // Check if the URL is changed to "/home" after clicking the home logo
+    expect(global.window.location.pathname).toBe("/home");
+  });
+
+  test("clicking Popular redirects to Popular movies page", async () => {
+    render(
+      <BrowserRouter>
         <SearchContext.Provider value={{ setSearchText: mockSetSearchText }}>
           <RouterWrapper>
             <Header />
           </RouterWrapper>
         </SearchContext.Provider>
-      </MemoryRouter>
+      </BrowserRouter>
     );
 
-    act(() => {
-    userEvent.click(screen.getByAltText("Home"));
-    expect(screen.getByTestId("location-display")).toHaveTextContent("/");
-    });
+    const popularButton = screen.getAllByText("Popular")[0];
+    fireEvent.click(popularButton);
+
+    expect(screen.getByTestId("location-display")).toHaveTextContent(
+      "/movies/popular"
+    );
   });
 
-  test('navigates to the search page when Search link is clicked', () => {
+  test("clicking Top Rated redirects to Top Rated movies page", async () => {
+    render(
+      <BrowserRouter>
+        <SearchContext.Provider value={{ setSearchText: mockSetSearchText }}>
+          <RouterWrapper>
+            <Header />
+          </RouterWrapper>
+        </SearchContext.Provider>
+      </BrowserRouter>
+    );
+
+    const popularButton = screen.getAllByText("Top Rated")[0];
+    fireEvent.click(popularButton);
+
+    expect(screen.getByTestId("location-display")).toHaveTextContent(
+      "/movies/top_rated"
+    );
+  });
+
+  test("clicking Upcoming redirects to Upcoming movies page", async () => {
+    render(
+      <BrowserRouter>
+        <SearchContext.Provider value={{ setSearchText: mockSetSearchText }}>
+          <RouterWrapper>
+            <Header />
+          </RouterWrapper>
+        </SearchContext.Provider>
+      </BrowserRouter>
+    );
+
+    const popularButton = screen.getAllByText("Upcoming")[0];
+    fireEvent.click(popularButton);
+
+    expect(screen.getByTestId("location-display")).toHaveTextContent(
+      "/movies/upcoming"
+    );
+  });
+
+  test("navigates to the search page when Search link is clicked", () => {
     const setSearchText = jest.fn();
-  
+
     render(
       <MemoryRouter>
         <SearchContext.Provider value={{ setSearchText }}>
@@ -68,12 +138,15 @@ describe("Header", () => {
         </SearchContext.Provider>
       </MemoryRouter>
     );
-  
+
     act(() => {
-    userEvent.type(screen.getByPlaceholderText('search movie name...'), 'test search query');
-    userEvent.click(screen.getByRole('search-button'));
+      userEvent.type(
+        screen.getByPlaceholderText("search movie name..."),
+        "test search query"
+      );
+      userEvent.click(screen.getByRole("search-button"));
     });
-  
-    expect(setSearchText).toHaveBeenCalledWith('test search query');
+
+    expect(setSearchText).toHaveBeenCalledWith("test search query");
   });
 });
