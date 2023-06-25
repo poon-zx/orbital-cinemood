cdcc3242b4b605e23071e7dce34e34633bb2a025
kayeugenia <122983473+kayeugenia@users.noreply.github.com>
2023-06-26 03:27:35 +0800

    separate components reply

--- src/components/Forum/forum.js
+++ src/components/Forum/forum.js
@@ -9,8 +9,8 @@ import { useAuth } from '../../context/AuthProvider.jsx';
 import WriteReview from "../../modals/writeReview.js";
 import Rating from "../../modals/rating.js";
 import Delete from "../../modals/delete.js"
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import { SettingsOverscanRounded } from "@mui/icons-material";
+import Replies from "./replies.js";
+import ReplyForm from "./replyForm.js";
 
 const Forum = ({ movieId }) => {
     const [currentMovieReview, setMovieReview] = useState([]);
@@ -151,125 +151,5 @@ const Forum = ({ movieId }) => {
     );
 };
 
-const ReplyForm = ({ reviewId, handleReply }) => {
-    const [replyContent, setReplyContent] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (replyContent.trim() !== "") {
-            handleReply(reviewId, replyContent);
-            setReplyContent("");
-        }
-    };
-
-    return (
-        <form className="reply-form" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Write a reply..."
-                value={replyContent}
-                onChange={(e) => setReplyContent(e.target.value)}
-            />
-            <button type="submit">Reply</button>
-        </form>
-    );
-};
-
-const Replies = ({ reviewId, deleteReply = false}) => {
-    const [replies, setReplies] = useState([]);
-    const auth = useAuth();
-
-    useEffect(() => {
-        fetchReplies();
-    }, [reviewId, replies, deleteReply]);
-
-    const fetchReplies = async () => {
-        try {
-            const { data, error } = await supabase
-                .from("reply")
-                .select(`*, 
-                    user:user_id (email, username)`
-                )
-                .eq("review_id", reviewId);
-
-            if (error) {
-                console.error("Error fetching replies:", error.message);
-                return;
-            }
-
-            if (data && data.length > 0) {
-                setReplies(data);
-            }
-        } catch (error) {
-            console.error("Error fetching replies:", error.message);
-        }
-    };
-
-    const handleDelete = async (replyId) => {
-        try {
-            const { data, error } = await supabase
-                .from("reply")
-                .delete()
-                .eq("id", replyId);
-
-            if (error) {
-                console.error("Error deleting reply:", error.message);
-                return;
-            }
-
-            if (data) {
-                deleteReply = true;
-                fetchReplies();
-            }
-        } catch (error) {
-            console.error("Error deleting reply:", error.message);
-        }
-    };
-
-    return (
-        <div className="replies">
-            {replies.length > 0 ? (
-                replies.map((reply) => (
-                    <div className="reply" key={reply.id}>
-                        <div className="reply__container">
-                            <Avatar />
-                            <div className="reply__first">
-                                <span>
-                                    <span className="reply__user__email">
-                                    {reply.user.username ? reply.user.username : reply.user.email}
-                                    </span>
-                                    <span className="reply__date">
-                                        {new Date (reply.created_at).toLocaleString("en-US", {
-                                        year: "numeric",
-                                        month: "2-digit",
-                                        day: "2-digit",
-                                        hour: "2-digit",
-                                        minute: "2-digit",
-                                        second: "2-digit",
-                                        hour12: true
-                                        })}
-                                    </span>
-                                </span>
-                                <span className="delete-icon-button">
-                                    {reply.user_id === auth.user.id ?
-                                    <IconButton
-                                        className="delete-icon-button"
-                                        onClick={() => handleDelete(reply.id)}
-                                    >
-                                        <DeleteOutlineOutlinedIcon className="edit-icon" />
-                                    </IconButton> : ""
-                                    }
-                                </span>
-                            </div>
-                        </div>
-                        <CardText className="reply__user__content">{reply.content}</CardText>
-                    </div>
-                ))
-            ) : (
-                <div className="no-replies">No replies yet.</div>
-            )}
-        </div>
-    );
-};
 
 export default Forum;
